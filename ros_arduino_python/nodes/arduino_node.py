1858633427ef84d652d09df334ccef98bf361fb3
Sandeepan Ghosh <sandtgbg@gmail.com>
2023-10-29 13:13:11 +0530

    track

--- ros_arduino_python/nodes/arduino_node.py
+++ ros_arduino_python/nodes/arduino_node.py
@@ -92,7 +92,8 @@ class ArduinoROS():
 
         # Start polling the sensors and base controller
         while not rospy.is_shutdown():
-
+            # msg=self.controller.get_encoder_counts()
+            # print(msg)
             r.sleep()
 
     
